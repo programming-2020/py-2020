71a64dd7ad146643af59fe996a908f3a5554fc0c
ti17m006 <ti17m006@technikum-wien.at>
2020-04-05 19:02:56 +0200

    Simple examples

---
+++ 01-variables/01-integer.py
@@ -0,0 +1,15 @@
+# Example of integer numbers
+number_one = 1
+number_two = 2
+number_five = 5
+
+print(number_one)
+print(number_two)
+print(number_five)
+print("****")
+
+number_three = number_one + number_two
+number_four = number_two + number_two
+number_ten = number_two * number_five
+number_eight = number_ten - number_five
+number_seven = number_four + number_three
---
+++ hello.py
@@ -0,0 +1,4 @@
+print("Hello :)")
+print("*" * 4)
+x = 1
+y = 2
