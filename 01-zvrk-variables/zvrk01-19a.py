460c48293ac7132dd72913472a2167536d3deeea
zvrgit <ntsinbox@mail.ru>
2020-04-08 21:23:09 +0200

    Zvrki added 4 new files

---
+++ 01-zvrk-variables/zvrk01-19a.py
@@ -0,0 +1,8 @@
+print("Start")
+print("----")
+a = True
+b = False
+
+output = a or b and not a
+print("Output je = " , output)
+print("----")
---
+++ 01-zvrk-variables/zvrk01-19c.py
@@ -0,0 +1,8 @@
+print("Start")
+print("----")
+a = True
+b = False
+
+output = not a and b
+print("Output je = " , output)
+print("----")
