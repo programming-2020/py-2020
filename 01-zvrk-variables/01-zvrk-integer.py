35b66098ae5ba8e6894c8c1622ab3f531adcfda3
zvrgit <ntsinbox@mail.ru>
2020-04-08 14:32:25 +0200

    Zvrki modified

--- 01-zvrk-variables/01-zvrk-integer.py
+++ 01-zvrk-variables/01-zvrk-integer.py
@@ -6,9 +6,37 @@ number_one = 1
 number_two = 2
 number_five = 5
 
+print("***")
+print(number_one)
+print(number_two)
+print(number_five)
 
-number_three
-number_four
-number_ten
-number_eight
-number_seven
+number_three = number_five - number_two
+number_four = number_two * number_two
+number_ten = number_two * number_five
+number_eight = number_four * number_two
+number_seven = number_five + number_two
+
+print("***")
+print(number_three)
+print(number_four)
+print(number_ten)
+print(number_eight)
+print(number_seven)
+
+print("***")
+div_two = number_five / number_two
+print("5 / 2")
+print(div_two)
+
+print("***")
+div_two_integer = number_five // number_two
+print("5 // 2")
+print(div_two_integer)
+
+print("***")
+mod_three = number_seven % number_four
+print("7 % 4")
+print(mod_three)
+print("***")
+
