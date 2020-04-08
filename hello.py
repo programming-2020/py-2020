676785f2edb0a87f53cd1967e4fb9020e9a7ae10
zvrgit <ntsinbox@mail.ru>
2020-04-08 12:12:41 +0200

    Live Share Test

--- hello.py
+++ hello.py
@@ -3,8 +3,9 @@ print("*" * 4)
 x = 1
 y = 2
 
+z = x + y
+print(z)
+print("Da li vidis?")
 print("Zvrki Branch")
-print("Zvrki Branch")
-print("Zvrki Branch")
-print("Zvrki Branch")
-print("Zvrki Branch")
+
+print("Vidm, vidim")
