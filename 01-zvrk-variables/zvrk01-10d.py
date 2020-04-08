284e66a58d564427b67f9230e24c57be01e1ada2
zvrgit <ntsinbox@mail.ru>
2020-04-08 20:07:22 +0200

    Zvrki added new file

---
+++ 01-zvrk-variables/zvrk01-10d.py
@@ -0,0 +1,9 @@
+print("Start")
+print("----")
+
+x = int(input("x = "))
+y = int(input("y = "))
+z = int(input("z = "))
+
+output = (x + y - z) / ((2 * x) + (y**2)  - (x * y * z))
+print("Output = ", output)
