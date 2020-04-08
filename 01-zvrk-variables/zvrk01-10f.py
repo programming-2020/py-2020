752024de1d53e287166b1f80f2eec702797ce82a
zvrgit <ntsinbox@mail.ru>
2020-04-08 21:05:18 +0200

    Zvrki added new file

---
+++ 01-zvrk-variables/zvrk01-10f.py
@@ -0,0 +1,11 @@
+print("Start")
+print("----")
+import math
+
+x = int(input("x = "))
+y = int(input("y = "))
+z = int(input("z = "))
+
+output = 1 + (x / math.factorial(2)) + (y / math.factorial(3)) + (z / math.factorial(4))
+print("Output = ", output)
+print("----")
