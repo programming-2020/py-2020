4d8959aa8119488336b2d662ac974a9da6bffb6a
ti17m006 <ti17m006@technikum-wien.at>
2020-04-09 17:45:43 +0200

    chapter01-21c

--- 01-vars-and-ops/chapter01-21c.py
+++ 01-vars-and-ops/chapter01-21c.py
@@ -1,1 +1,17 @@
 from math import sqrt
+
+
+print('Point (px, py)')
+px = float(input('px = '))
+py = float(input('py = '))
+
+ox = 1
+oy = 0
+
+r = sqrt(ox ** 2 + oy ** 2)
+pr = sqrt((ox - px) ** 2 + (oy - py) ** 2)
+
+output = r <= pr
+
+message = f'Output = {output}'
+print(message)
