b7ab36118f07cef76b121289f6ab7acb38fcc3b6
ti17m006 <ti17m006@technikum-wien.at>
2020-04-09 12:20:19 +0200

    chapter01-18b

--- 01-vars-and-ops/chapter01-18b.py
+++ 01-vars-and-ops/chapter01-18b.py
@@ -1,9 +1,14 @@
 
-x
-t
-y
+x = int(input('x = '))
+t = int(input('t = '))
+y = int(input('y = '))
 
 first = x >= 0
-second = t
+second = t  # False: None, 0, "", '', [], {}, ()
 third = x % 2 == 0
 fourth = y * y != 4
+
+output = first or second and third or fourth
+
+message = f'Output = {output}'
+print(message)
