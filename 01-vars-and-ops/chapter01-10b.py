d773deae80c6c1c8a455b5f9d84f5c75bceb24b9
ti17m006 <ti17m006@technikum-wien.at>
2020-04-09 11:32:35 +0200

    chapter01-10b

--- 01-vars-and-ops/chapter01-10b.py
+++ 01-vars-and-ops/chapter01-10b.py
@@ -1,7 +1,11 @@
-import math
 
 x = int(input("x = "))
 
 y = int(input("y = "))
 
-output =
+xsqr = x ** 2
+ysqr = y ** 2
+output = (xsqr + ysqr) ** 2
+
+message = f'Output = {output}'
+print(message)
