a3457e36bc4061cd626b3baffd8fa38505e8a838
ti17m006 <ti17m006@technikum-wien.at>
2020-04-09 12:36:33 +0200

    chapter01-19ab

--- 01-vars-and-ops/chapter01-19a.py
+++
@@ -1,8 +0,0 @@
-
-a = True
-b = False
-
-output = a or b and not a
-
-message = f'Output = {output}'
-print(message)
---
+++ 01-vars-and-ops/chapter01-19ab.py
@@ -0,0 +1,20 @@
+
+a = True
+b = False
+
+output_a = a or b and not a
+output_b = (a or b) and not a
+
+#   True or False -> True
+#   not True -> False
+#   True and False -> False
+
+# !! python boolean precedence: not, and, or
+
+# not True -> False
+# False and False -> False
+# True or False -> True
+
+
+message = f'Output a = {output_a}\nOutput b = {output_b}'
+print(message)
