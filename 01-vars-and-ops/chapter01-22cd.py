ab6c895ee38cf6a2c6b18f8d15f2d552f36bcb71
ti17m006 <ti17m006@technikum-wien.at>
2020-04-10 19:46:40 +0200

    chapter01-22cd

---
+++ 01-vars-and-ops/chapter01-22cd.py
@@ -0,0 +1,17 @@
+
+number_minus_one = -1
+number_one = 1
+number_two = 2
+number_five = 5
+
+x = float(input("x = "))
+
+first_interval = number_two <= x <= number_five
+second_interval = number_minus_one <= x <= number_one
+
+output_a = first_interval or second_interval
+output_b = not (first_interval or second_interval)
+output_c = first_interval and second interval
+
+message = f'Output = {}'
+print(message)
