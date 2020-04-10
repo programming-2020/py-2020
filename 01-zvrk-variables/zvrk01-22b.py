b1e81d5b164cd62965a74ea834e672b5b4322e25
zvrgit <ntsinbox@mail.ru>
2020-04-10 19:19:36 +0200

    Modified zvrk01-22b

--- 01-zvrk-variables/zvrk01-22b.py
+++ 01-zvrk-variables/zvrk01-22b.py
@@ -6,7 +6,7 @@ number_one = 1
 
 x = float (input ("x = "))
 
-output_out = number_zero < x and number_one < x or number_zero > x
+output_out = x < number_zero or number_one < x 
 message = f"Output = {output_out}"
 print (message)
 print ("----")
