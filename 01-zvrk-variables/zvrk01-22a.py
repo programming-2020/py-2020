78ad37ab137a1d694fb742c58e8287b6894efb70
zvrgit <ntsinbox@mail.ru>
2020-04-10 18:48:19 +0200

    Added zvrk01-22a and zvrk01-22b

---
+++ 01-zvrk-variables/zvrk01-22a.py
@@ -0,0 +1,13 @@
+print ("Start")
+print("----")
+
+number_zero = 0
+number_one = 1
+
+x = int (input ("x = "))
+
+output_in = x >= number_zero and x <= number_one
+
+message = f"Output = {output_in}"
+print (message)
+print ("----")
---
+++ 01-zvrk-variables/zvrk01-22b.py
@@ -0,0 +1,13 @@
+print ("Start")
+print("----")
+
+number_zero = 0
+number_one = 1
+
+x = float (input ("x = "))
+
+output_out = number_zero < x and number_one < x  
+
+message = f"Output = {output_out}"
+print (message)
+print ("----")
