c6cabce0852dee9b529b0a2f174a65fc544929af
ti17m006 <ti17m006@technikum-wien.at>
2020-04-10 07:51:42 +0200

    chapter01-22ab.py

--- 01-vars-and-ops/chapter01-22ab.py
+++ 01-vars-and-ops/chapter01-22ab.py
@@ -1,9 +1,13 @@
-
+number_zero = 0
+number_one = 1
 
 x = float(input('x = '))
 
-output_in = (x >= 0) and (x <= 1)
+output_in = (x >= number_zero) and (x <= number_one)
 output_out = not output_in
 
-message = f'Output in = {output_in}\nOutput out = {output_out}'
+output_in_b = number_zero <= x <= number_one
+output_out_b = not output_in_b 
+
+message = f'Output in = {output_in}\nOutput out = {output_out}\n\nOutput in other notation = {output_in_b}\nOutput out other notation = {output_out_b}'
 print(message)
