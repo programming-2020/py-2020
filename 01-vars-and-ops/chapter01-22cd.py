23a789ec6f242a2f04ff8fcb1db8ffda88383a4a
ti17m006 <ti17m006@technikum-wien.at>
2020-04-10 20:04:51 +0200

    chapter01-22cd

--- 01-vars-and-ops/chapter01-22cd.py
+++ 01-vars-and-ops/chapter01-22cd.py
@@ -9,11 +9,12 @@ x = float(input("x = "))
 first_interval = number_two <= x <= number_five
 second_interval = number_minus_one <= x <= number_one
 
+# output = not (( x >= number_two and x <= number_five ) or ( x >= number_minus_one and x <= number_one ))
+
 output_a = first_interval or second_interval
 output_b = not (first_interval or second_interval)
 output_c = first_interval and second interval
 
-# output = not (( x >= number_two and x <= number_five ) or ( x >= number_minus_one and x <= number_one ))
 
 message = f'Output = {}'
 print(message)
