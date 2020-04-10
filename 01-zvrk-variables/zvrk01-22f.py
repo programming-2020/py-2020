4eee6dbca0a6311ef62dc3515b880c09b9317112
zvrgit <ntsinbox@mail.ru>
2020-04-10 20:45:39 +0200

    Added zvrk01-22f

---
+++ 01-zvrk-variables/zvrk01-22f.py
@@ -0,0 +1,14 @@
+print("Start")
+print("----")
+
+number_zero = 0
+
+x = int (input ("x = "))
+y = int (input ("y = "))
+z = int (input ("z = "))
+
+output = (x > number_zero) and (y > number_zero) and (z > number_zero)
+
+message = f"Output = {output}"
+print(message)
+print("----")
