3a5f58dffa8bc4f9724da171a102909855ab92fa
ti17m006 <ti17m006@technikum-wien.at>
2020-04-17 17:59:57 +0200

    functions

---
+++ 02-functions/01-function.py
@@ -0,0 +1,26 @@
+def message():
+	text = "Hello";
+	print(text);
+
+
+def my_message(name):
+	text = f'Hello {name}'
+	print(text)
+	return;
+	print('New text');
+
+my_message('ABC')
+
+def pi():
+	return 3.141529
+
+
+def area_circle(r):
+	return r ** 2 * pi()
+
+
+def odd(n):
+	return n % 2 != 0
+
+print(not odd(432))
+
---
+++ 02-functions/02-function.py
@@ -0,0 +1,13 @@
+
+def odd(n):
+	return n % 2 !=0 
+
+
+a = int(input("a = "))
+
+
+message = f'Odd = {odd(a)}'
+print(message)
+
+
+
