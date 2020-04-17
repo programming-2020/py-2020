a04a3c501ffc5974893ad3af1be5b5298d97e188
ti17m006 <ti17m006@technikum-wien.at>
2020-04-17 17:58:42 +0200

    chess

--- 01-vars-and-ops/chapter02-27.py
+++ 01-vars-and-ops/chapter02-27.py
@@ -4,6 +4,7 @@
 # b) does the queen in a field A attacks field B
 # c) does the knight in a field A attaks field B
 
+two = 2
 
 xa = int(input('xa = '))
 ya = int(input('ya = '))
@@ -11,4 +12,9 @@ ya = int(input('ya = '))
 xb = int(input('xb = '))
 yb = int(input('yb = '))
 
+black_a = (xa % two == 0) and (ya % two != 0)
+black_b = (xb % two == 0) and (ya % two != 0)
+
+
+output_a = black_a == black_b
 
