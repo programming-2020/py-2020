5f9466a6ff827c213490e50f8a801cc25b036122
ti17m006 <ti17m006@technikum-wien.at>
2020-04-25 18:48:55 +0200

    odd example

--- 02-functions/02-function.py
+++ 02-functions/02-function.py
@@ -1,13 +1,18 @@
 
 def odd(n):
-	return n % 2 !=0 
+    return n % 2 != 0
 
+
+def odd_message(n):
+    if odd(n):
+        return f'{n} is odd'
+    return f'{n} is even'
+
+
+###################
 
 a = int(input("a = "))
-
-
-message = f'Odd = {odd(a)}'
+message = f'Odd = {odd_message(a)}'
 print(message)
 
-
-
+###########
