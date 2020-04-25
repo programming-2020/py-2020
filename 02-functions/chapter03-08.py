686814efe9113b9d2f60347ba029fe6a229c3e79
ti17m006 <ti17m006@technikum-wien.at>
2020-04-25 19:22:32 +0200

    example 8

---
+++ 02-functions/chapter03-08.py
@@ -0,0 +1,24 @@
+def positive(n):
+    return n > 0
+
+
+def sum(a, b, c):
+    s = int()
+    if (positive(a)):
+        s += a
+    if (positive(b)):
+        s += b
+    if (positive(c)):
+        s += c
+    return s
+
+
+def sum_message(a, b, c):
+    return f'sum = {sum(a, b, c)}'
+
+
+a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+
+print(sum_message(a, b, c))
