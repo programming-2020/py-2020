822079e66bada2600dc1568628d89feea130c61c
ti17m006 <ti17m006@technikum-wien.at>
2020-04-25 19:54:16 +0200

    example 10

---
+++ 02-functions/chapter03-10.py
@@ -0,0 +1,36 @@
+zero = 0
+one = 1
+two = 2
+three = 3
+five = 5
+
+
+def first(n):
+    return (n < zero)
+
+
+def second(n):
+    return (zero <= n < one)
+
+
+def third(n):
+    return (one <= n < five)
+
+
+def fourth(n):
+    return n >= five
+
+
+def y_value(x):
+    if (first(x)):
+        return -five
+    elif (second(x)):
+        return x + two
+    elif (third(x)):
+        return three * x - one
+    else:
+        return two * x
+
+
+x = int(input('x = '))
+print(f'y = {y_value(x)}')
