f894d42eceff1663a572b9adf708e6ea06a07512
ti17m006 <ti17m006@technikum-wien.at>
2020-04-25 19:34:22 +0200

    example 9

---
+++ 02-functions/chapter03-09.py
@@ -0,0 +1,25 @@
+def isZero(n):
+    zero = 0
+    return n == zero
+
+
+def check(a, b, c):
+    counter = int()
+    if (isZero(a)):
+        counter += 1
+    if (isZero(b)):
+        counter += 1
+    if (isZero(c)):
+        counter += 1
+    return counter
+
+
+def check_message(a, b, c):
+    return f'Values equal zero: {check(a, b, c)}'
+
+
+a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+
+print(check_message(a, b, c))
