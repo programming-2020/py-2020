14ea0ebeb5360eafb0a5654bf31d61d35d090fa6
ti17m006 <ti17m006@technikum-wien.at>
2020-05-03 11:46:59 +0200

    chapter03-20

--- 02-functions/chapter03-20.py
+++ 02-functions/chapter03-20.py
@@ -1,1 +1,25 @@
 # solve the equation ax+b=0
+
+
+zero = 0
+
+
+def check(a):
+    return a != 0
+
+
+def my_function(a, b):
+    if check(a):
+        return -(b / a)
+    return zero
+
+def message(a, b):
+    x = my_function(a, b)
+    if x != 0:
+        return f"x = {x}"
+    return "x = error"
+
+a = int(input("a = "))
+b = int(input("b = "))
+
+print(message(a, b))
