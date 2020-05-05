e30a392bda42f97e755266ab177c2ea2c3a7dd1b
ti17m006 <ti17m006@technikum-wien.at>
2020-05-05 19:11:29 +0200

    factoriel

--- 02-functions/chapter04-12.py
+++ 02-functions/chapter04-12.py
@@ -4,15 +4,23 @@ one = 1
 def element(n, i ,m):
     return n + i * m
 
-def product(n, m):
+def product_a(n, m):
     p = one
     for i in range(zero, m + one):
         p *= element(n, i, m)
     return p
 
+def product_b(n, m):
+    signum = -one
+    p = one
+    m += 1
+    for i in range(n, m):
+        p *= (n + (signum ** m) * i * (m - one))
+    return p
+         
 
 n = int(input("n = "))
 m = int(input("m = "))
 
-message = f'P {product(n, m)}'
+message = f'P {product_b(n, m)}'
 print(message)
