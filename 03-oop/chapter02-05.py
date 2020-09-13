ab08dacc4202e006ad2c506aa76171dc169979d9
githubdmn <dmninbox@gmail.com>
2020-09-13 15:55:54 +0200

    point class as a module

--- 03-oop/chapter02-05.py
+++ 03-oop/chapter02-05.py
@@ -6,6 +6,10 @@ a = Point("A", 4, 5)
 print(a.printPoint())
 print(f"Distance from 0 = {a.distance()}")
 
+# change object property value
+a.x = 2
+print(a.printPoint())
+
 a.reposition(3, 4)
 print(a.printPoint())
 print(f"Distance from 0 = {a.distance()}")
