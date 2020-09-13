1190492c880deae03d90d45b125b5833b6b0fe2b
githubdmn <dmninbox@gmail.com>
2020-09-13 18:52:26 +0200

    box and parcel

--- 03-oop/packages/pack07/parcel.py
+++ 03-oop/packages/pack07/parcel.py
@@ -1,5 +1,20 @@
 
-from box import Box
+# from box import Box
+
+class Box(object):
+    '''Box class'''
+
+    def __init__(self, label, length, width, height):
+        self.__label = label
+        self.__length = length
+        self.__width = width
+        self.__height = height
+
+    def volume(self):
+        return self.__length * self.__width * self.__height
+
+    def show(self):
+        return f"Box: {self.__label} Length: {self.__length} Width: {self.__width} Height: {self.__height}"
 
 
 class Parcel(Box):
