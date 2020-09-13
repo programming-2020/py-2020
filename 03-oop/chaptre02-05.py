1d256a9ddc3b8eb5fba0550db5b68c20a90ed8e4
githubdmn <dmninbox@gmail.com>
2020-09-13 14:29:37 +0200

    modules

---
+++ 03-oop/chaptre02-05.py
@@ -0,0 +1,3 @@
+from packages.pack05.point import Point
+
+help(Point('A', 3, 4))
---
+++ 03-oop/packages/pack05/point.py
@@ -0,0 +1,9 @@
+
+class Point:
+    '''Coordinate system point object\n
+    Point has name (string), x (integer), y (integer)'''
+
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
