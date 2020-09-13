28ed8080e99d91e0f689a44424056b040f8055e9
githubdmn <dmninbox@gmail.com>
2020-09-13 18:36:23 +0200

    inheritance example

--- 03-oop/chapter02-07.py
+++ 03-oop/chapter02-07.py
@@ -1,1 +1,6 @@
-# '''Inheritance example'''
+# '''Inheritance example with overriding'''
+
+from packages.pack07.parcel import Parcel
+
+a = Parcel("First parcel", 5, 4, 3, 700, "Cardbord")
+print(a.show())
