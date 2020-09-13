5530655834e19ea8c78df927b3dba5fa90233279
githubdmn <dmninbox@gmail.com>
2020-09-13 14:36:00 +0200

    module pack05 point

--- 03-oop/packages/pack05/point.py
+++ 03-oop/packages/pack05/point.py
@@ -1,4 +1,7 @@
 
+from math import sqrt
+
+
 class Point:
     '''Coordinate system point object\n
     Point has name (string), x (integer), y (integer)'''
@@ -7,3 +10,13 @@ class Point:
         self.name = name
         self.x = x
         self.y = y
+
+    def reposition(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self):
+        return sqrt(self.x ** 2 + self.y ** 2)
+
+    def printPoint(self):
+        return f"Point {self.name} ({self.x}, {self.y})"
