cc60a21b3b7769190942b6cf77268e64c8a71c80
githubdmn <dmninbox@gmail.com>
2020-10-14 14:23:03 +0200

    Variable argument example

--- 02-functions/06-args.py
+++ 02-functions/06-args.py
@@ -2,6 +2,7 @@
 The special syntax *argv in function definitions in python is used to pass a variable number of arguments to a function. It is used to pass a non-key worded, variable-length argument list. 
 *argv is positioned as a last parameter in a function/method or second to last
 if the **kwargs is in place.
+A single asterisk (*) to unpack iterables.
 '''
 
 
