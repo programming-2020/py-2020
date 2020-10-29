fdd477ffb1f84fd9fad6447cccd322af75790eae
githubdmn <dmninbox@gmail.com>
2020-10-29 11:33:58 +0100

    Properties

---
+++ 03-oop/chapter04-01.py
@@ -0,0 +1,30 @@
+
+'''
+Properties simple example
+'''
+
+
+class Contact:
+
+    def __init__(self, fullname, email):
+        self.__fullname = fullname
+        self.__email = email
+
+    def _setName(self, fullname):
+        if not fullname:
+            raise Exception("The name is invalid")
+        self.__fullname = fullname
+
+    def _getName(self):
+        return self.__fullname
+
+    # Order is important - first get, second set
+    fullname = property(_getName, _setName)
+
+
+a = Contact("ABCD", "abc@mail.com")
+print(a.fullname)
+a.fullname = "New One"
+print(a.fullname)
+a.fullname = ""
+print(a.fullname)
