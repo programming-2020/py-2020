0a17321d9a69513decb030978a913790d5c7e5a9
githubdmn <dmninbox@gmail.com>
2020-11-25 12:50:26 +0100

    point - class example

---
+++ 03-oop/tutorial01- 03-class.py
@@ -0,0 +1,24 @@
+
+
+class Point():
+    """
+    class point with x and y properties
+    """
+
+    def __init__(self, name, x, y):
+        """
+        __init__ (initialisation method) is a magic method
+        """
+        self.name = name
+        self.x = x
+        self.y = y
+
+    def show(self):
+        """
+            Shows point properties -> name(x, y) 
+        """
+        return f"{self.name}({self.x}, {self.y})"
+
+
+a = Point("A", 4, 5)
+print(a.show())
